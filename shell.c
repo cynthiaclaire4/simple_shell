b389a9ab73bb52695cff8cbff888b9355e227bd1
cynthiaclaire4 <you@example.com>
2022-05-15 15:22:59 +0100

    pipe

---
+++ shell.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include "main.h"
+
+int main(int ac, char **av)
+{
+	char *cmdstr = NULL;
+	char **args = NULL;
+
+	while(1)
+	{
+		if (ac > 1)
+			args = av + 1;
+		else
+		{
+			/* read command from user */
+			cmdstr = execute_line();
+			/*change command into arg of strings */
+			args = to_args(cmdstr);
+		}
+
+		/*execute commands */
+		execve(args[0], args, NULL);
+
+		dprintf(STDERR_FILENO, "%s: command not found\n", args[0]);
+	}
+	return(0);
+}
