154771c9f2c5301b9677308ce2edd1d9bfc246e1
Praize-Jha18 <praisejahfrancis@gmail.com>
2022-05-20 08:19:55 -0700

    Shell commit

--- env_builtins.c
+++ env_builtins.c
@@ -14,10 +14,10 @@ int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
  * @front: A double pointer to the beginning of args.
  *
  * Return: If an error occurs - -1.
- * 	   Otherwise - 0.
- * 
+ * Otherwise - 0.
+ *
  * Description: Prints one variable per line in the
- *		format 'variable'='value'.
+ * format 'variable'='value'.
  */
 int shellby_env(char **args, char __attribute__((__unused__)) **front)
 {
@@ -42,9 +42,9 @@ int shellby_env(char **args, char __attribute__((__unused__)) **front)
  * @args: An array of arguments passed to the shell.
  * @front: A double pointer to the beginning of args.
  * Description: args[1] is the name of the new or existing PATH variable.
- * 		args[2] is the value to set the new or changed variable to.
+ * args[2] is the value to set the new or changed variable to.
  * Return: If an error occurs - -1.
- * 	   Otherwise - 0.
+ * Otherwise - 0.
  */
 int shellby_setenv(char **args, char __attribute__((__unused__)) **front)
 {
@@ -97,7 +97,7 @@ int shellby_setenv(char **args, char __attribute__((__unused__)) **front)
  * Description: args[1] is the PATH variable to remove.
  *
  * Return: If an error occurs - -1.
- * 	   Otherwise - 0.
+ * Otherwise - 0.
  */
 int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front)
 {
